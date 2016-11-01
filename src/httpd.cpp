b8da959b2d295ebbb9a4ad1c34a1ab9fa11e7230
jack <jack.wgm@gmail.com>
2016-11-01 12:21:21 +0800

    add CMakeLists.txt

--- src/httpd.cpp
+++ src/httpd.cpp
@@ -7,7 +7,6 @@
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
-// g++ httpd.cpp -static-libstdc++ -static-libgcc -pthread -Wl,-Bstatic -lboost_system -lboost_thread -lboost_filesystem -lboost_coroutine -lboost_context -o httpd
 //
 
 #include <cstdlib>
